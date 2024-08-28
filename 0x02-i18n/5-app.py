3b53eef8b8825f13ae74ec48c1c18d15483359ba
lumanya <shaibulumanya@gmail.com>
2024-08-28 12:40:32 +0300

    [ADD] mock loggin

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Mock logging in"""
+
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config(object):
+    """_summary_
+
+    Returns:
+                    _type_: _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """Returns a user dictionary or None if the ID cannot be found"""
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    user = get_user()
+    g.user = user
+
+
+@app.route('/')
+def index():
+    """hello world"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
